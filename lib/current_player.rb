29ccfebbcabc4c4907cba7fbdb6a02882ae519c0
Jennifer Silva <jennifersilva20@gmail.com>
2019-08-07 07:43:58 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,4 +2,7 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def turn_count(board)
   turns = 0
   board.each do |spot|
-    if spot == X
+    if spot == "X" or spot == "O"
+      turns += 1 
+    end 
+  end
